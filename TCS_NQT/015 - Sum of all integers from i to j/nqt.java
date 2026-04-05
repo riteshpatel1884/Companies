078e3c0cecada5dd0358100091d298796eec8330
Ritesh Patel <129499361+riteshpatel1884@users.noreply.github.com>
2026-04-05 15:07:40 +0530

    Add files via upload

---
+++ TCS_NQT/015 - Sum of all integers from i to j/nqt.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class nqt {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long i = sc.nextLong();
+    long j = sc.nextLong();
+
+    // Handle if i > j
+    if (i > j) {
+      long temp = i;
+      i = j;
+      j = temp;
+    }
+
+    // handle constraint if j>9999
+    if(j>9999){
+      System.out.println("Invalid value of j");
+    }
+    long n = j - i + 1;
+
+    long sum = n * (i + j) / 2;
+
+    System.out.println(sum);
+
+  }
+}
